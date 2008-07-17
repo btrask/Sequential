b4283479ead028a0a54072ba3019631880e4c505
pi <pi@Mini.local>
2008-07-17 11:55:26 -0500

    Accidentally added a bunch of bunk files.

--- libxad MODIFIED/portable/cvsbuild.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-./cvsclean.sh
-autoreconf -i
-mkdir _install
-./configure --prefix=`pwd`/_install --enable-debug
-make
-make install
--- libxad MODIFIED/portable/cvsclean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-topdir=`pwd`
-for x in `find . -name .cvsignore`
-do
-  cd `dirname $x`
-  rm -vrf `cat .cvsignore`
-  cd $topdir
-done
